524b508484605fa3c5dc036b8b73969e08c0e364
bomi <a51022866@gmail.com>
2018-10-05 21:38:31 +0900

    added code

--- javaBasic/src/org/sopt/interfaceExample/UserServiceImpl.java
+++ javaBasic/src/org/sopt/interfaceExample/UserServiceImpl.java
@@ -1,5 +1,22 @@
 package org.sopt.interfaceExample;
 
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
 
+    // 필수 구현 메소드
+    @Override
+    public void getInfo() {
+
+    }
+
+    // 필수 구현 메소드
+    @Override
+    public String getDept() {
+        return null;
+    }
+
+    // 선택 구현 메소드
+    @Override
+    public void setState(boolean state) {
+        // 재정의해서 사용할 수 있다.
+    }
 }
