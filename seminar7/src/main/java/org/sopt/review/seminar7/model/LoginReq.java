4199f39fcfd361e00511438ef7bba6c761e31bad
bomi <a51022866@gmail.com>
2018-12-14 18:27:35 +0900

    create LoginReq

---
+++ seminar7/src/main/java/org/sopt/review/seminar7/model/LoginReq.java
@@ -0,0 +1,13 @@
+package org.sopt.review.seminar7.model;
+
+import lombok.Data;
+
+/**
+ * Created by bomi on 2018-12-14.
+ */
+
+@Data
+public class LoginReq {
+    private String email;
+    private String password;
+}
