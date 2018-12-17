42186c3a2485e1bcea0a475f7251a0ce6b045953
bomi <a51022866@gmail.com>
2018-12-17 14:54:52 +0900

    Finish mapper about user

---
+++ seminar7/src/main/java/org/sopt/review/seminar7/mapper/UserMapper.java
@@ -0,0 +1,34 @@
+package org.sopt.review.seminar7.mapper;
+
+import org.apache.ibatis.annotations.*;
+import org.sopt.review.seminar7.dto.User;
+import org.sopt.review.seminar7.model.SignUpReq;
+
+/**
+ * Created by bomi on 2018-12-17.
+ */
+
+@Mapper
+public interface UserMapper {
+    // 로그인
+    @Select("SELECT userIdx, name, email, password, profileUrl FROM user WHERE email = #{email} AND password = #{password}")
+    User findByEmailAndPassword(@Param("email") final String email, @Param("password") final String password);
+
+    // 회원 조회
+    @Select("SELECT userIdx, name, email, password, profileUrl FROM user WHERE userIdx = #{userIdx}")
+    User findUser(@Param("userIdx") final int userIdx);
+
+    // 회원 가입
+    @Insert("INSERT INTO user(name, email, password, profileUrl) VALUES(#{signUpReq.name}, #{signUpReq.email}, #{signUpReq.password}, #{signUpReq.profile}")
+    @Options(useGeneratedKeys = true, keyColumn = "user.userIdx")
+    void signUpUser(@Param("signUpReq") final SignUpReq signUpReq);
+
+    // 회원 정보 수정
+    @Update("UPDATE user SET name = #{signUpReq.name}, email = #{signUpReq.email}, password = #{signUpReq.password}, profile = #{signUpReq.profile} WHERE userIdx = #{userIdx}")
+    User updateUser(@Param("userIdx") final int userIdx, @Param("signUpReq") final SignUpReq signUpReq);
+
+    // 회원 탈퇴
+    @Delete("DELETE FROM user WHERE userIdx = #{userIdx}")
+    void deleteUser(@Param("userIdx") final int userIdx);
+
+}
